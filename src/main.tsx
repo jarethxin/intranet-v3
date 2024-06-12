e52ece8bcdf20fbb7d66c08eb4e34aa69be5ef6a
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-12 18:02:59 -0500

    UP

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/globals.css'
-import Home from '@/pages/home'
+import { Home } from '@/pages/home'
 import { Toaster } from "@/components/ui/toaster"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
