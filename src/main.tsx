3b5d302d202fcaff1a09bc9e695d46d43b0d9a51
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-12 15:59:14 -0500

    Files upload

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="bg-blue-500 text-white p-4 rounded-lg">
+      Hola, mundo!:D
+    </div>
+  </React.StrictMode>,
+)
