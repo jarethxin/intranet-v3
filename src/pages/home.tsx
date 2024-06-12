8a9173dbb160e42798ff9669dadd0c831701cfe4
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-12 17:44:47 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   const { toast } = useToast();
 
   return (
-    <section className="bg-red-500 dark:bg-gray-900">
-      <div className="container border-2 border-black p-4">
-        <Button
-          onClick={() => {
-            toast({
-              title: "Hey!",
-              description: ":D!",
-            });
-          }}
-        >
-          ?
-        </Button>
-      </div>
-    </section>
+    <>
+      <section className="bg-red-500 dark:bg-gray-900">
+        <div className="flex flex-col m-4 justify-center">
+          <NavigationMenu className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                  <Button
+                    onClick={() => {
+                      toast({
+                        title: "Hey!",
+                        description: ":D!",
+                      });
+                    }}
+                    className="text-base font-medium"
+                  >
+                    Click me!
+                  </Button>
+                <NavigationMenuContent>Link</NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </section>
+    </>
   );
 }
