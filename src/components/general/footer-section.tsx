cfa6a0c3738c5b345de1f6b70e4a05dace8b96bb
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-13 13:30:52 -0500

    UP
    Creación de animación para elementos Hoverables

--- src/components/general/footer-section.tsx
+++ src/components/general/footer-section.tsx
@@ -1,7 +1,27 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import { Button } from "../ui/button";
+
 export function Footer() {
   return (
     <section className="bg-gray-900 text-white py-20">
       <div className="container mx-auto px-4">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild className="ml-10 mb-5">
+              <Button className="border border-gray-700 hover:bg-gray-700 text-white font-bold rounded">
+                Hover
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent className="TooltipContent" >
+              <p className="text-sm px-3 py-2 mb-1 border border-gray-700 rounded">Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <h1 className="text-xl font-bold">Welcome to Footer Section</h1>
         <p className="text-sm mb-4">
           This is a brief description for the footer section.
