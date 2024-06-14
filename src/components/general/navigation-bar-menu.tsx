9aa2fdd1791e990bf2c8e7a674e70ff4f12e58f6
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-14 11:27:01 -0500

    Update navigation-bar-menu.tsx

--- src/components/general/navigation-bar-menu.tsx
+++ src/components/general/navigation-bar-menu.tsx
@@ -13,38 +13,42 @@ export function NavigationBarMenu() {
   const { toast } = useToast();
 
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem className="cursor-pointer">
-          <Link to="/">
-            <img
-              src={logoImg}
-              alt="Logo"
-              className="h-10 w-auto m-1 cursor-pointer"
-              onClick={() => {
-                toast({
-                  title: "Hey",
-                  description: ":D",
-                });
-              }}
-            />
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="cursor-pointer">
-          <Link to="/contacto">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Contacto
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="cursor-pointer">
-          <Link to="/login">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Login
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="sticky top-0 z-50 w-full bg-white bg-opacity-70">
+      <div className="container mx-auto flex items-center space-x-4 px-4">
+        <NavigationMenu className="flex-grow">
+          <NavigationMenuList className="flex items-center space-x-4">
+            <NavigationMenuItem className="cursor-pointer">
+              <Link to="/">
+                <img
+                  src={logoImg}
+                  alt="Logo"
+                  className="h-10 w-auto m-1 cursor-pointer"
+                  onClick={() => {
+                    toast({
+                      title: "Hey",
+                      description: ":D",
+                    });
+                  }}
+                />
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="cursor-pointer">
+              <Link to="/contacto">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contacto
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="cursor-pointer">
+              <Link to="/login">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Login
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
   );
 }
